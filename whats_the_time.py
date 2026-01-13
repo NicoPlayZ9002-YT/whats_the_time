7a9c99806cbb8ed6b2562043f1a720223bc1113b
NicoPlayZ9002-YT <ngestal9000@gmail.com>
2026-01-13 16:05:47 +0100

    Update whats_the_time.py

--- whats_the_time.py
+++ whats_the_time.py
@@ -4,9 +4,9 @@ def give_time(formatted=True, time_format=24):
 	curr: float = time.time()
 	if formatted == True:
 		curr_time = time.localtime(curr)
-		if format == 24:
+		if time_format == 24:
 			return time.strftime("%H:%M:%S", curr_time)
-		elif format == 12:
+		elif time_format == 12:
 			return time.strftime("%I:%M:%S %p", curr_time)
 		else:
 			print("ArgError: time_format must be 24 or 12")
