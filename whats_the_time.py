471e41e7e85bc9411552fae88749b63b41914a6a
NicoPlayZ9002-YT <ngestal9000@gmail.com>
2026-03-24 16:55:14 +0100

    Update whats_the_time.py

--- whats_the_time.py
+++ whats_the_time.py
@@ -2,14 +2,13 @@ import time,sys
 
 def give_time(formatted=True, time_format=24):
 	curr: float = time.time()
-	if formatted == True:
+	if formatted:
 		curr_time = time.localtime(curr)
 		if time_format == 24:
 			return time.strftime("%H:%M:%S", curr_time)
 		elif time_format == 12:
 			return time.strftime("%I:%M:%S %p", curr_time)
 		else:
-			print("ArgError: time_format must be 24 or 12")
-			sys.exit(127)
+			raise ValueError("time_format must be 24 or 12")
 	else:
 		return curr
